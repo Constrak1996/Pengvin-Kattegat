00ba025a0a671fe389974b4f1ff32e96028779a2
Lucas' Laptop <lucas@romanini.dk>
2019-03-27 10:48:58 +0100

    Update UI.cs
    Added fields and properties for player health and score

--- Pengvin Pjat/Assets/Scripts/UI.cs
+++ Pengvin Pjat/Assets/Scripts/UI.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Field and property for the players health
+    private int health = 3;
+    public int Health { get => health; set => health = value; }
+
+    //Field and property for the score
+    private int score;
+    public int Score { get => score; set => score = value; }
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
-        
+        if(Health <= 0)
+        {
+            
+        }
     }
 }
