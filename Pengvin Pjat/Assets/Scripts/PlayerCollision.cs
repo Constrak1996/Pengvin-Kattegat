8c0859774afe469c988d0cd77fa3939db64ed140
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2019-03-27 15:59:24 +0100

    ohbwed

--- Pengvin Pjat/Assets/Scripts/PlayerCollision.cs
+++ Pengvin Pjat/Assets/Scripts/PlayerCollision.cs
@@ -8,6 +8,10 @@ public class PlayerCollision : MonoBehaviour
 
     public int Health { get => health; set => health = value; }
 
+    private bool takingDmg;
+
+    private float immortalTime;
+
     public int fishCounter = 0;
 
     // Start is called before the first frame update
@@ -28,8 +32,9 @@ public class PlayerCollision : MonoBehaviour
         {
             Debug.Log("We hit an obstacle");
             Health--;
+            takingDmg = true;
             Debug.Log(Health);
-            Destroy(collision.gameObject);
+            //Destroy(collision.gameObject);
         }
         else if (collision.tag == "Fish")
         {
