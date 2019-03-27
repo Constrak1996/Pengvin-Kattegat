b1b364922d5ba27d89cc4606f3be58d96fb72e36
Lucas' Laptop <lucas@romanini.dk>
2019-03-27 15:45:39 +0100

    Menu merged into master

--- Pengvin Pjat/Assets/Scripts/MainMenu.cs
+++ Pengvin Pjat/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
     
 }
