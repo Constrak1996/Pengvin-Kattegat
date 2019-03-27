9467597f503f21921cfd7a7618ac5faff94e3140
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2019-03-27 12:37:21 +0100

    Tilføjet obstacles der bevæger sig fra højre til venstre

--- Pengvin Pjat/Assets/Scripts/ObstacleMovement.cs
+++ Pengvin Pjat/Assets/Scripts/ObstacleMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class ObstacleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(speed, 0, 0);
     }
 }
