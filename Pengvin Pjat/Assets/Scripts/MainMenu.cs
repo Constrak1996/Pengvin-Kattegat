2c2bd3bb03db6cc4a1ed71d982b787fdd1306895
Lucas' Laptop <lucas@romanini.dk>
2019-03-29 10:07:28 +0100

    Mute function added, needs to be updated when sound is added

--- Pengvin Pjat/Assets/Scripts/MainMenu.cs
+++ Pengvin Pjat/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource asource;
+    bool soundMute;
+
+    public void start()
+    {
+        //asource = GetComponent<AudioSource>();
+        soundMute = false;
+    }
     // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
-    
+
+    public void Mute()
+    {
+        //asource.mute =! asource.mute;
+        if(soundMute == false)
+        {
+            soundMute = true;
+            Debug.Log("Sounds has been muted");
+        }
+        else
+        {
+            soundMute = true;
+            Debug.Log("Sounds has been unmuted");
+        }
+
+    }
+ 
 }
