e534c8d588d005f6bd74f609ee09c146311f722c
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2019-04-02 13:59:34 +0200

    Tilføjet deathwall igen igen

--- Pengvin Pjat/Assets/Scripts/ObstacleS/SixPackTrashMovement.cs
+++ Pengvin Pjat/Assets/Scripts/ObstacleS/SixPackTrashMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SixPackTrashMovement : MonoBehaviour
 {
     private float speed = -0.015f;
+
     // Start is called before the first frame update
     void Start()
     {
