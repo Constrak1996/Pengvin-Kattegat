88848eab846e422acc426c345e4f91333a0a7e77
Vaknez <alexandertomassen@live.dk>
2019-04-03 15:06:18 +0200

    fixed my shit

---
+++ Pengvin Pjat/Assets/Scripts/Bomb/NevalMinemovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NevalMinemovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale == 1)
+        {
+            transform.Translate(speed, 0, 0);
+
+            if (transform.position.x <= -13)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+
+        }
+
+    }
+}
